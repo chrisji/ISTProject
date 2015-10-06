24a6df5abd1b3b1a75448fdfd08328c7a12d569e
Chris Inskip <chrisji@hotmail.co.uk>
2015-10-06 15:26:26 +0100

    Document (and tidy)  class.

--- src/main/java/checkers/Move.java
+++ src/main/java/checkers/Move.java
@@ -1,14 +1,20 @@
 package checkers;
 
 /**
+ * Data structure representing a `move` (a transition from one cell on the board to another).
+ *
+ * This class also contains helper functions for comparing `Move` equality, checking to see if this move
+ * was a capturing move, and obtaining the co-ordinates of the piece that this move captured (in the case
+ * of a capturing move).
+ *
  * @author Chris Inskip
  * @version 03/10/2015
  */
 public class Move {
-    private int fromRow;
-    private int fromCol;
-    private int toRow;
-    private int toCol;
+    private final int fromRow;
+    private final int fromCol;
+    private final int toRow;
+    private final int toCol;
 
     public Move(int fromRow, int fromCol, int toRow, int toCol) {
         this.fromRow = fromRow;
@@ -17,43 +23,72 @@ public class Move {
         this.toCol = toCol;
     }
 
+    /**
+     * Returns the row that the move is transitioning the piece from.
+     *
+     * @return the row that the move is transitioning the piece from.
+     */
     public int getFromRow() {
         return fromRow;
     }
 
+    /**
+     * Returns the column that the move is transitioning the piece from.
+     *
+     * @return the column that the move is transitioning the piece from.
+     */
     public int getFromCol() {
         return fromCol;
     }
 
+    /**
+     * Returns the row that the move is transitioning the piece to.
+     *
+     * @return the row that the move is transitioning the piece to.
+     */
     public int getToRow() {
         return toRow;
     }
 
+    /**
+     * Returns the column that the move is transitioning the piece to.
+     *
+     * @return the column that the move is transitioning the piece to.
+     */
     public int getToCol() {
         return toCol;
     }
 
+    /**
+     * Returns true if this move has resulted in a piece being captured, false otherwise.
+     *
+     * @return true if this is a capturing move, false otherwise
+     */
     public boolean isTake() {
-        // Returns true if there was a jump
         return (Math.abs(fromRow - toRow) > 1) && (Math.abs(fromCol - toCol) > 1);
     }
 
-    public String toString() {
-        return String.format("(%d, %d) -> (%d, %d) take=%b", fromRow, fromCol, toRow, toCol, isTake());
-    }
-
+    /**
+     * Returns the row of the piece that was taken by performing this move.
+     *
+     * @return the row of the piece that was taken by performing this move.
+     */
     public int calcTakenRow() {
-        if (fromRow < toRow) {
-            return fromRow + 1;
-        }
-        return fromRow - 1;
+        return (fromRow + toRow) / 2;
     }
 
+    /**
+     * Returns the column of the piece that was taken by performing this move.
+     *
+     * @return the column of the piece that was taken by performing this move.
+     */
     public int calcTakenCol() {
-        if (fromCol < toCol) {
-            return fromCol + 1;
-        }
-        return fromCol - 1;
+        return (fromCol + toCol) / 2;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%d, %d) -> (%d, %d) isTake=%b", fromRow, fromCol, toRow, toCol, isTake());
     }
 
     @Override
