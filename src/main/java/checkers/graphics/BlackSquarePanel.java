d7f81b852f97c408cf0f834ebbed3b196694def9
Chris Inskip <chrisji@hotmail.co.uk>
2015-11-29 13:43:27 +0000

    Tidy and document `BlackSquarePanel`

--- src/main/java/checkers/graphics/BlackSquarePanel.java
+++ src/main/java/checkers/graphics/BlackSquarePanel.java
@@ -13,6 +13,7 @@ import java.io.IOException;
  */
 public class BlackSquarePanel extends SquarePanel {
 
+    // Checker image
     private BufferedImage image;
 
     // Colours
@@ -20,38 +21,31 @@ public class BlackSquarePanel extends SquarePanel {
     private final Color SELECTED_BACKGROUND_COLOUR = new Color(50, 50, 50);
     private final Color SELECTED_BORDER_COLOUR = new Color(60, 100, 60);
 
+    /**
+     * Constructor for empty squares
+     */
     public BlackSquarePanel() {
         this.setBackground(BACKGROUND_COLOUR);
     }
 
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-
-        int gap = 5; // Gap between image and size of square.
-        g.drawImage(image, gap, gap, this.getWidth()-2*gap, this.getHeight()-2*gap, null);
-    }
-
+    /**
+     * Constructor for non-empty squares
+     *
+     * @param isRed `true` if the piece is red, `false` for black
+     * @param isCrowned `true` if the piece is crowned, `false` for uncrowned
+     */
     public BlackSquarePanel(boolean isRed, boolean isCrowned) {
-        System.out.println("Adding piece black square: RED=" + isRed + " CROWNED=" + isCrowned);
+        System.out.println("Adding piece to black square (RED=" + isRed + ", CROWNED=" + isCrowned + ")");
         this.setBackground(BACKGROUND_COLOUR);
 
         try {
             if (isRed && isCrowned) { // Crowned red
-//                this.add(new JLabel("R")); // TODO REMOVE THIS DEBUG LINE
-//                this.setBackground(new Color(250, 140, 150)); // TODO REMOVE THIS DEBUG LINE
                 image = ImageIO.read(new File("res/red-checker-crowned.png"));
             } else if (isRed) { // Normal red
-//                this.add(new JLabel("r")); // TODO REMOVE THIS DEBUG LINE
-//                this.setBackground(new Color(250, 140, 150)); // TODO REMOVE THIS DEBUG LINE
                 image = ImageIO.read(new File("res/red-checker.png"));
             } else if (isCrowned) { // Crowned black
-//                this.add(new JLabel("B")); // TODO REMOVE THIS DEBUG LINE
-//                this.setBackground(new Color(130, 130, 130)); // TODO REMOVE THIS DEBUG LINE
                 image = ImageIO.read(new File("res/black-checker-crowned.png"));
             } else { // Normal Black
-//                this.add(new JLabel("b")); // TODO REMOVE THIS DEBUG LINE
-//                this.setBackground(new Color(130, 130, 130)); // TODO REMOVE THIS DEBUG LINE
                 image = ImageIO.read(new File("res/black-checker.png"));
             }
         } catch (IOException e) {
@@ -59,13 +53,30 @@ public class BlackSquarePanel extends SquarePanel {
         }
     }
 
+    /**
+     * Changes the appearance of the square to show that it is selected.
+     */
     public void select() {
         this.setBackground(SELECTED_BACKGROUND_COLOUR);
         this.setBorder(BorderFactory.createLineBorder(SELECTED_BORDER_COLOUR, 3));
     }
 
+    /**
+     * Reverts the square back to its default appearance, i.e. before it was selected
+     */
     public void deselect() {
         this.setBackground(BACKGROUND_COLOUR);
         this.setBorder(BorderFactory.createEmptyBorder());
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        int imageBorder = 5; // Gap between image and size of square.
+        int imageWidth = this.getWidth() - (2*imageBorder);
+        int imageHeight = this.getHeight() - (2*imageBorder);
+
+        g.drawImage(image, imageBorder, imageBorder, imageWidth, imageHeight, null);
     }
 }
